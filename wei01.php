4d31cbce9ecf0e3105364af943de3d035b0d7dc1
wei <showtime0718@gmail.com>
2017-05-17 16:35:15 +0800

    20170517v1

--- wei01.php
+++ wei01.php
@@ -5,7 +5,15 @@
  * Date: 2017/5/17
  * Time: 下午 03:57
  */
+echo 'hello world</br>';
+echo 'hello world</br>';
+echo 'hello world</br>';
 echo 'hello world';
+
+
+
+
+
 ?>
-<h1>YOYO</h1>
+
 
---
+++ wei02.php
@@ -0,0 +1,7 @@
+<?php
+$a = 123;
+$_123 = 12.3;
+$___ = tuer;
+$qoo = 'cool';
+var_dump($qoo);
+?>
